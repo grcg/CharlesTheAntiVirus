9d9114761cace4b4d50860d815bd0df6f4602272
grcg <grcg0@yahoo.com>
2018-01-06 23:10:29 -0500

    Edits unnecessary files.

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -64,7 +64,7 @@ public class PlayerHealth : MonoBehaviour
 					//GetComponent<PlayerControl>().enabled = false;
 
 					// ... disable the Gun script to stop a dead guy shooting a nonexistant bazooka
-					GetComponentInChildren<Gun>().enabled = false;
+					//GetComponentInChildren<Gun>().enabled = false;
 
 					// ... Trigger the 'Die' animation state
 					anim.SetTrigger("Die");
